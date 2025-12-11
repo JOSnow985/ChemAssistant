ef0d1f3d3c7522d61a7170f31d284c60a7b5d7f5
JOSnow985 <jaden.olvera@students.snow.edu>
2025-12-10 22:45:26 -0700

    Menu number control and demo

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿// Jaden Olvera, CS-1400, Final Project: Chem Assistant
 
-// Store paths as variables we can edit easily later if need be
 using System.Diagnostics;
 
+// Store paths as variables we can edit easily later if need be
 string retrieveURL = "placeholder";
 string pTableFile = "ptable.csv";
 
@@ -21,12 +21,56 @@ else
     pTableList = new List<List<string>>();
 }
 
-// The List retrieved from the file should be 118 elements long with a header line
+// The pTable List retrieved from the file should be 118 elements long with a header line
 Debug.Assert(pTableList.Count == 119);
+
+string[] mainMenuArray = [  "1. Molar Mass Calculator",
+                                "2. Bookmarks",
+                                "3. Element Look Up",
+                                "4. Exit"
+                        ];
+
+bool exiting = false;
+while (exiting == false)
+{
+    switch (SelectMenu(mainMenuArray))
+    {
+        case 0:
+            Console.WriteLine("0");
+            exiting = true;
+            break;
+        case 1:
+            Console.WriteLine("1");
+            exiting = true;
+            break;
+        case 2:
+            Console.WriteLine("2");
+            exiting = true;
+            break;
+        case 3:
+            Console.WriteLine("3");
+            exiting = true;
+            break;
+        case 4:
+            Console.WriteLine("4");
+            exiting = true;
+            break;
+        default:
+            Console.WriteLine("d");
+            exiting = true;
+            break;
+            
+    }
+}
+
+
+
+
+
 
 // -------------------------------- Methods --------------------------------
 
-// Checks if we already have a ptable.csv to use, if we don't, retrieves it
+// Checks if we already have a ptable.csv to use, if we don't, ask for permission to retrieve it
 static async Task fileGrab(string retrieveURL, string outputFilePath)
 {
     if (File.Exists("ptable.csv") == false)
@@ -70,31 +114,24 @@ static async Task fileGrab(string retrieveURL, string outputFilePath)
     }
 }
 
-// Main Menu that returns a value to indicate what the requested function is
-static int mainMenu(List<List<string>> dataList, int userIndex)
+static void drawHeader()
 {
-    string[] mainMenuArray = [  "1. Check Balance", 
-                                "2. Withdraw", 
-                                "3. Deposit", 
-                                "4. Display Last 5 Transactions", 
-                                "5. Quick Withdraw $40", 
-                                "6. Quick Withdraw $100",
-                                "7. End Current Session"
-                            ];
-    int firstOptionIndex = 2;
-    // Return selected option, offset by 1 to account for length of array
-    return optionSelector(mainMenuArray, firstOptionIndex) - 1;
+    Console.Clear();
+    Console.WriteLine("ChemAssistant");
+    Console.WriteLine();
 }
 
-// Displays an array to the user and lets the user control which one is highlighted
-// Requires a value to indicate where the first selectable option is in the string array
-// Up and Down arrows and Enter to select, returns selected option
-static int optionSelector(string[] menuArray, int firstOptionIndex)
+// Menu system that returns a value to indicate what the requested function is
+static int SelectMenu(string[] menuArray)
 {
-    int optionHighlighted = firstOptionIndex;
-    while (true)
+    int optionHighlighted = 0;
+    bool selectionMade = false;
+
+    // Displays an array to the user and lets the user control which one is highlighted
+    // Up and Down arrows and Enter to select, returns selected option
+    while (selectionMade == false)
     {
-        // drawHeader();
+        drawHeader();
         for (int index = 0; index < menuArray.Length; index++)
         {
             // If this index is our current selection, highlight it
@@ -114,13 +151,28 @@ static int optionSelector(string[] menuArray, int firstOptionIndex)
             }
         }
         ConsoleKey userInput = Console.ReadKey(true).Key;
-        if (userInput == ConsoleKey.UpArrow && optionHighlighted > firstOptionIndex)
+        // Casts the pressed key to an integer value we can use to check if they pressed a number key
+        int userInputValue = (int)userInput - 48;
+
+        // Then checks if that number is a possible choice on the menu
+        if (userInputValue >= 0 && userInputValue < menuArray.Length)
+        {
+            optionHighlighted = userInputValue;
+            selectionMade = true;
+        }
+        else if (userInput == ConsoleKey.UpArrow && optionHighlighted > 0)
             optionHighlighted--;
-        else if (userInput == ConsoleKey.DownArrow && optionHighlighted <= menuArray.Length - firstOptionIndex)
+        else if (userInput == ConsoleKey.DownArrow && optionHighlighted < menuArray.Length - 1)
             optionHighlighted++;
         else if (userInput == ConsoleKey.Enter)
-            return optionHighlighted;
+        {
+            optionHighlighted++;
+            selectionMade = true;
+        }
         else
             Console.Beep();
     }
+
+    // Return selected option, offset by 1 to account for zero indexing the array
+    return optionHighlighted;
 }
